af1f8a91df9d051c25a4072984883f066358a205
Ryan Burbank <rburbank@streamsend.com>
2015-07-26 13:34:34 -0700

    Add upvote & downvote functions to Post and Comment schemas

--- models/Posts.js
+++ models/Posts.js
@@ -7,4 +7,14 @@ var PostSchema = new mongoose.Schema({
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
 
+PostSchema.methods.upvote = function(callback) {
+  this.upvotes += 1;
+  this.save(callback);
+};
+
+PostSchema.methods.downvote = function(callback) {
+  this.upvotes -= 1;
+  this.save(callback);
+};
+
 mongoose.model('Post', PostSchema);
